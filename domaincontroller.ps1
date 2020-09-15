8980fb077494e0b3fdb26816972ba0d02bbdf371
u565620 <ctatro@footlocker.com>
2020-09-15 08:20:01 -0500

    test'

--- domaincontroller.ps1
+++ domaincontroller.ps1
@@ -2,3 +2,4 @@
 & winrm set winrm/config/client '@{TrustedHosts="*"}'
 & winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 & winrm set winrm/config/client/auth '@{Basic="true"}'
+& netsh advfirewall set allprofiles state off
