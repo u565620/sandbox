e3322daf245e04d01f829161def9c433e80cf3ee
u565620 <ctatro@footlocker.com>
2020-09-15 06:32:04 -0500

    initial commit

---
+++ domaincontroller.ps1
@@ -0,0 +1,4 @@
+& winrm set winrm/config/service/auth '@{Basic="true"}'
+& winrm set winrm/config/client '@{TrustedHosts="*"}'
+& winrm set winrm/config/service '@{AllowUnencrypted="true"}'
+& winrm set winrm/config/client/auth '@{Basic="true"}'
