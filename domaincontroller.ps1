a3ef3db358bb203d208a034c654cbb9df6c777f7
u565620 <ctatro@footlocker.com>
2020-09-15 09:53:50 -0500

    test'

--- domaincontroller.ps1
+++ domaincontroller.ps1
@@ -3,3 +3,8 @@
 & winrm set winrm/config/service '@{AllowUnencrypted="true"}'
 & winrm set winrm/config/client/auth '@{Basic="true"}'
 & netsh advfirewall set allprofiles state off
+Add-WindowsCapability -Online -Name OpenSSH.Client~~~~0.0.1.0
+Add-WindowsCapability -Online -Name OpenSSH.Server~~~~0.0.1.0
+Start-Service sshd
+Set-Service -Name sshd -StartupType 'Automatic'
+& ssh-keygen
